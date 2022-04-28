e4e6eef73949ae9447a6bab6abb787cf1b7f9f82
Vernyoon Chao <vernchao510@gmail.com>
2022-04-28 10:46:02 -0700

    validation for empty name or password

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,6 +4,20 @@ const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 const router = express.Router();
 
 // Restore session user
@@ -19,6 +33,7 @@ router.get("/", restoreUser, (req, res) => {
 // Log in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
