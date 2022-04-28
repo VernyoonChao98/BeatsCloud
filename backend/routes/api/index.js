ab5f5287dbfb2556fdcd1fcaa44fa482cc796668
Vernyoon Chao <vernchao510@gmail.com>
2022-04-28 11:38:57 -0700

    custom csrfFetch

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,10 +10,6 @@ const { requireAuth } = require("../../utils/auth.js");
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
-
 router.get(
   "/set-token-cookie",
   asyncHandler(async (_req, res) => {
