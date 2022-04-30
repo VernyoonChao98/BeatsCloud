5985aa0a23083699b4e838d3b4a33397392fcf10
Vernyoon Chao <vernchao510@gmail.com>
2022-04-30 14:16:51 -0700

    installed audio player

--- frontend/src/components/PersonalHome/index.js
+++ frontend/src/components/PersonalHome/index.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Redirect, NavLink } from "react-router-dom";
+import { restoreUser } from "../../store/session";
 import Navigation from "./Navigation";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import "./PersonalHome.css";
 
 function PersonalHome() {
   const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreUser());
+  }, [dispatch]);
 
   if (!sessionUser) return <Redirect to="/" />;
 
